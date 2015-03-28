4e49068ace4f7b3f48035c9b161ea3afb11570c5
MICHAEL HAZANI <123mickey@gmail.com>
2015-03-28 12:26:47 -0700

    trends, ASCII art, comments, cleanp

--- twit.py
+++ twit.py
@@ -1,34 +1,52 @@
+# -*- coding: utf-8 -*-
+
+
+# ToDo:
+# GetTrendsCurrent --DONE
+
+
 import sys
 import twitter
 from colorama import init
-init(strip=not sys.stdout.isatty()) # strip colors if stdout is redirected
+init(strip=not sys.stdout.isatty())  # strip colors if stdout is redirected
 from termcolor import cprint
 from pyfiglet import figlet_format
+reload(sys)
+sys.setdefaultencoding("utf8")
 
-# import time
+# get access to the Api with michaelhazani's keys
+api = twitter.Api(consumer_key='wrNczJOfwAVb5g7UdOENTEgK0',
+                  consumer_secret='XwKCnpTfZKASNaGUWUT3wtCcTcTtLiHNpXN1F0YcfgT'
+                                  'zp4dzXT',
+                  access_token_key='3124478422-6puXKn7aPN69LJQzYXfsexHt9NBi9rs'
+                                  'NAZyOM61',
+                  access_token_secret='pE9blQ7F6NMkbAY6bFAnUW7ExN2rKVrhszu44OH'
+                                  'nk8nRo')
 
 
-api = twitter.Api(consumer_key='Sac39Y6C6fJ3yAcX3xme80Hz7',
-consumer_secret='bHeBpGBFJFe66kRXcHeQWU6TKp6uiu7QO9wTCX83lw8B2lJMpM',
-access_token_key='189239506-XsfaRhqtPja2Qt9Xpc2CleLtNgvuxPYBMLA5nPWn',
-access_token_secret='Bdd4pRQNNEoCv1V4vtZyGyjyh4ke9wgbFSPe0YAIhjOxa')
-
-
+# Main Menu and question
 def mainmenu():
+    """Start Screen"""
     print(chr(27) + "[2J")
-    cprint(figlet_format('Twit!', font='doh'), 'white', 'on_blue', attrs=['bold'])
+    cprint(figlet_format('TWiT!', font='doh'), 'white', 'on_blue',
+           attrs=['bold'])
     print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
     print "Welcome to Twit! A little thing for Twitter."
     menuquestion()
 
+
 def menuquestion():
+    """Prompt the user to choose an option"""
     print "What would you like to do?"
-    print "Select (S)earch keywords, get (U)ser statuses or (Q)uit"
+    print ("Select (S)earch keywords, get (U)ser statuses, "
+           "see current (T)rends or (Q)uit")
     menuchoice = raw_input(">").upper()
     if menuchoice == "S":
         search()
     elif menuchoice == "U":
         userstatuses()
+    elif menuchoice == "T":
+        trends()
     elif menuchoice == "Q":
         print(chr(27) + "[2J")
         quit()
@@ -39,8 +57,13 @@ def menuquestion():
         menuquestion()
 
 
-
+# Search for Terms
 def search():
+    """Search for specific terms"""
+    print(chr(27) + "[2J")
+    cprint(figlet_format('Keyword Search!', font='big'), 'white', 'on_blue',
+           attrs=['bold'])
+    print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
     query = raw_input("Enter Twitter search term: \n>")
     resultnum = raw_input("How many results? \n>")
     if not resultnum:
@@ -49,16 +72,32 @@ def search():
     resultlist = [x.text for x in results]
     print "\n".join(resultlist)
     print "\n\n\n\n\n\n\n"
-    choice = raw_input("Now what? Options: (N)ew Search, (M)ain page\n>").upper()
+    searchprompt()
+
+
+def searchprompt():
+    choice = raw_input("Now what? Options: "
+                       "(N)ew Search, (M)ain page\n>").upper()
     if choice == "N":
         print(chr(27) + "[2J")
         search()
-    if choice == "M":
+    elif choice == "M":
         print(chr(27) + "[2J")
         mainmenu()
+    elif choice != "N" and choice != "M":
+        print "\n"
+        print "Please enter M or N"
+        print "\n"
+        searchprompt()
 
 
+# Get User Statuses
 def userstatuses():
+    """Get statuses from specific users"""
+    print(chr(27) + "[2J")
+    cprint(figlet_format('Find Statuses', font='big'), 'white', 'on_yellow',
+           attrs=['bold'])
+    print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
     chosenuser = raw_input("Whose statuses do you want to check out? \n@")
     # cuformatted = "\'%s\'" % chosenuser
     print "Searching for statuses by %s" % chosenuser
@@ -68,12 +107,50 @@ def userstatuses():
     # friends = api.GetFriends()
     # print [u.name for u in friends]
     print "\n\n\n\n\n\n\n"
+    statusprompt()
+
+
+def statusprompt():
     choice = raw_input("Now what? Options: (N)ew user, (M)ain page\n>").upper()
     if choice == "N":
         print(chr(27) + "[2J")
         userstatuses()
-    if choice == "M":
+    elif choice == "M":
         print(chr(27) + "[2J")
         mainmenu()
+    elif choice != "N" and choice != "M":
+        print "\n"
+        print "Please enter M or N"
+        print "\n"
+        statusprompt()
 
+
+# Get Trends
+def trends():
+    """Get current trends"""
+    print(chr(27) + "[2J")
+    cprint(figlet_format('Get Current Trends!', font='big'), 'white', 'on_red',
+           attrs=['bold'])
+    print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+    trendresults = api.GetTrendsCurrent()
+    trendformatted = " \n".join(str(e) for e in trendresults)
+    print trendformatted
+    print "\n\n\n\n\n\n\n"
+    trendsprompt()
+
+
+def trendsprompt():
+    choice = raw_input("Now what? Options: (R)epeat Trend Search, "
+                       "(M)ain page\n>").upper()
+    if choice == "R":
+        print(chr(27) + "[2J")
+        trends()
+    elif choice == "M":
+        print(chr(27) + "[2J")
+        mainmenu()
+    elif choice != "R" and choice != "M":
+        print "\n"
+        print "Please enter M or R"
+        print "\n"
+    trendsprompt()
 mainmenu()
